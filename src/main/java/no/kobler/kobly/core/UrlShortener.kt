79080b6f19ca8aa37bb71e33cc9b85bfc466e5cd
Jonathan Lillesæter <jonathan@kobler.no>
2021-04-21 13:14:02 +0200

    Added Kotlin support

---
+++ src/main/java/no/kobler/kobly/core/UrlShortener.kt
@@ -0,0 +1,13 @@
+package no.kobler.kobly.core
+
+import java.util.Optional
+
+class UrlShortener {
+    fun shorten(url: String?): String {
+        return ""
+    }
+
+    fun findUrl(id: String?): Optional<String> {
+        return Optional.empty()
+    }
+}
